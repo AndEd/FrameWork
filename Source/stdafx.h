2ff426108e77973dcfa402ba5698040693b05c71
AndEd <zx3211226@gmail.com>
2018-07-09 16:50:43 +0900

    Create stdafx.h

---
+++ Source/stdafx.h
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <d3dx9.h>
+
+#include <Windows.h>
+#include <ctime.h>
+#include <mmsystem.h>
+
+#pragma comment(lib, "winmm.lib")
+#pragma comment(lib, "d3dx9.lib")
+#pragma comment(lib, "d3d9.lib")
+
+//Source
+
+#include "CustomVertex.h"
